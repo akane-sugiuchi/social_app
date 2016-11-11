a23d5013f6897a5919a0db00ba83d97abc244869
Tomonori Hayashi <hyshtmnr6238@gmail.com>
2016-11-12 01:07:49 +0900

    update

--- application.go
+++ application.go
@@ -1,21 +1,24 @@
 package main
 
-
 import (
+  //フレームワーク関連パッケージ
   "github.com/labstack/echo"
   "github.com/labstack/echo/engine/standard"
   "github.com/labstack/echo/middleware"
 
-  //データベース関連
+  //データベース関連パッケージ
   "database/sql"
   _ "github.com/go-sql-driver/mysql"
 
+  //ディレクトリ
   "./tool"
   "./model"
 )
 
 func db_connect() *sql.DB {
-  db,err := sql.Open("mysql","root:tomonori@tcp(localhost:3306)/social_app")
+  //db,err := sql.Open("mysql","root:tomonori@tcp(localhost:3306)/social_app")
+  db,err := sql.Open("mysql","root:tomonori@tcp(52.196.55.156:3306)/social_app")
+
   if err != nil {
     panic(err.Error())
   }
@@ -33,9 +36,10 @@ func main(){
 
   //モデル
   e.Get("/json",tool.Res_json())
-  e.Get("/email",tool.Res_mysql())
   e.Get("/calender",model.Echo_event(db))
-  e.Get("/regist",model.Echo_regist(db))
+  e.Get("/calender/regist",model.Echo_regist(db))
+  e.Get("/calender/update",model.Echo_update(db))
+  e.Get("/calender/delete",model.Echo_delete(db))
 
   e.Post("/pull",tool.Auto_pull())
   //サーバー構築 ポート1323
--- tool/exec-test.go
+++ tool/exec-test.go
@@ -2,8 +2,7 @@ package tool
 
 import (
     "net/http"
-    _ "os/exec"
-    _ "time"
+    "os/exec"
     _ "fmt"
     "github.com/labstack/echo"
     "github.com/codeskyblue/go-sh"
@@ -11,12 +10,10 @@ import (
 
 func Auto_pull() echo.HandlerFunc {
     return func(c echo.Context) error{
-        sh.Command("git","pull").Run()
-        //str := "pkill -f application"
-        //err := exec.Command("sh","-c",str).Start()
-        //fmt.Println(err)
-        //sh.Command("pkill","-f","application")
-        //fmt.Println("aa")
-        return c.String(http.StatusOK,"hey")
+        sh.Command("git","status").Run()
+        sh.Command("sh","test.sh").Run()
+        cmd := exec.Command("sh", "test.sh")
+        cmd.Start()
+        return c.String(http.StatusOK,"")
     }
 }
